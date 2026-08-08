a0be66a95c86d50c9b686efb72b6f4c9380c93ee
Ruan Pablo <ruanpablomota23@gmail.com>
2026-08-07 21:28:45 -0300

    feat: adiciona classe Arqueiro

--- Rpg-Java/src/dominio/Arqueiro.java
+++ Rpg-Java/src/dominio/Arqueiro.java
@@ -1,4 +1,32 @@
 package dominio;
 
-public class Arqueiro {
+import interfaces.Atacante;
+
+public class Arqueiro extends Personagem implements Atacante {
+    public Arqueiro(String nome, int vida, int ataqueBase, Arma arma, int nivel) {
+        super(nome, vida, ataqueBase, arma, nivel);
+    }
+
+```
+    @Override
+    public void exibirStatus() {
+        System.out.println("=== Arqueiro ===");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Vida: " + getVida());
+        System.out.println("Ataque: " + getAtaqueBase());
+        System.out.println("Ataque Total: " + getAtaqueTotal());
+        System.out.println("Nível: " + getNivel());
+
+    }
+
+    @Override
+    public void atacar(Personagem alvo) {
+        int novaVida = Math.max(0, alvo.getVida() - getAtaqueTotal());
+        alvo.setVida(novaVida);
+
+        System.out.println(getNome() + " lançou uma flecha em" + alvo.getNome());
+        System.out.println("Vida restante de " + alvo.getNome() + ": " + alvo.getVida());
+    }
+```
+
 }
