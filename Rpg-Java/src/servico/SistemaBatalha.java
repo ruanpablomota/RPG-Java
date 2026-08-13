e9181935c5e6ef191e4f0a5ca8607f1b0df6a9c4
Ruan Pablo <ruanpablomota23@gmail.com>
2026-08-12 21:57:51 -0300

    feat: implementa a classe SistemaBatalha no pacote servico

---
+++ Rpg-Java/src/servico/SistemaBatalha.java
@@ -0,0 +1,11 @@
+package servico;
+
+import dominio.Personagem;
+
+public class SistemaBatalha {
+
+    public void atacar(Personagem atacante, Personagem alvo){
+        atacante.atacar(alvo);
+    }
+
+}
